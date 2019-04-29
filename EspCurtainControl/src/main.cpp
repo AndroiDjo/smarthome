88cb5820c70978b0adcf25b2795a15786d1b38e1
Evgeniy.Gerasimov <slash.krd@gmail.com>
2019-04-29 23:56:27 +0400

    доработал сохранение настроек в память

--- EspCurtainControl/src/main.cpp
+++ EspCurtainControl/src/main.cpp
@@ -21,7 +21,11 @@ struct Mqtt {
 
 boolean power = true;
 boolean is_running = false;
-boolean forward;
+boolean is_opened = false;
+boolean forward = true;
+boolean step_callback = false;
+boolean step_last_dir = forward;
+boolean first_load = true;
 
 int stepmaxspeed = 2000;
 int stepacceleration = 300;
@@ -46,28 +50,43 @@ void somethingWrong(char* msg) {
    }
 }
 
-void initPrivate() {
-  File file = SPIFFS.open(privateFile, "r");
-  StaticJsonDocument<256> doc;
-  
-  if (!file) {
-    somethingWrong("Failed to open private file");
-  } 
-
-  DeserializationError error = deserializeJson(doc, file);
-  if (error) {
+void saveJsonParams(JsonObject& json) {
+   File file = SPIFFS.open(configFile, "r");
+   DynamicJsonDocument doc(jsonSize);
+   boolean existconfig = false;
+   if (file) {   
+    DeserializationError error = deserializeJson(doc, file);
+    if (error) {
+      file.close();
+      somethingWrong("Failed to dsrlz config file");
+    }
     file.close();
-    somethingWrong("Failed to dsrlz private file");
-  }
-  file.close();
-
-  strlcpy(mqtt.host, doc["mqtt_server"], sizeof(mqtt.host));
-  strlcpy(mqtt.login, doc["mqtt_login"], sizeof(mqtt.login));
-  strlcpy(mqtt.pass, doc["mqtt_pass"], sizeof(mqtt.pass));
-  mqtt.port = doc["mqtt_port"];
+    for (JsonPair kv : json) {
+      doc[kv.key()] = kv.value();
+    }
+    existconfig = true;
+   }
+   serializeJsonPretty(doc, Serial);
+   File destFile = SPIFFS.open(configFile, "w");
+   boolean srlzbool;
+   if (existconfig) {
+     srlzbool = (serializeJson(doc, destFile) == 0);
+   } else {
+     Serial.println("config file not found, create new");
+     srlzbool = (serializeJson(json, destFile) == 0);
+   }
+   if (srlzbool) {
+    destFile.close();
+    somethingWrong("Failed to write to config file");
+   }
+   destFile.close();
 }
 
 void moveCurtains() {
+  if (first_load && is_running) {
+    somethingWrong("need calibrate!");
+  }
+  first_load = false;
   if (!power || is_running) {
     Serial.println("!power || is_running");
     return;
@@ -75,9 +94,9 @@ void moveCurtains() {
 
   stepper.setMaxSpeed(stepmaxspeed);
   stepper.setAcceleration(stepacceleration);
-  if (forward) {
+  if (forward && is_opened) {
     stepper.moveTo(stepmoveto);
-  } else {
+  } else if (!forward && !is_opened) {
     stepper.moveTo(0);
   }
 
@@ -95,6 +114,17 @@ void moveCurtains() {
     digitalWrite(MS2, LOW);
   }
 
+  if (stepper.distanceToGo() != 0) {
+    step_callback = true;
+    is_running = true;
+    step_last_dir = forward;
+
+    StaticJsonDocument<100> doc;
+    doc["is_running"] = true;
+    JsonObject jo = doc.as<JsonObject>();
+    saveJsonParams(jo);
+  }
+
   Serial.println(stepper.currentPosition());
   Serial.println(forward);
   Serial.println(stepmoveto);
@@ -102,21 +132,7 @@ void moveCurtains() {
   Serial.println(stepper.distanceToGo());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  DynamicJsonDocument doc(jsonSize);
-  DeserializationError error = deserializeJson(doc, payload, length);
-  if (error) {
-    somethingWrong("Failed to dsrlz mqtt message");
-  }
-
+void parseRequest(DynamicJsonDocument& doc) {
   if (doc.containsKey("power")) {
     power = doc["power"];
   }
@@ -137,10 +153,73 @@ void callback(char* topic, byte* payload, unsigned int length) {
     stepdel = doc["stepdel"];
   }
 
+  if (doc.containsKey("is_running")) {
+    is_running = doc["is_running"];
+  }
+
+  if (doc.containsKey("is_opened")) {
+    is_opened = doc["is_opened"];
+  }
+
   if(doc.containsKey("curtain")) {
      forward = doc["curtain"];
      moveCurtains();
   }
+}
+
+void loadJsonParams() {
+   File file = SPIFFS.open(configFile, "r");
+   DynamicJsonDocument doc(jsonSize);
+   if (file) {
+     DeserializationError error = deserializeJson(doc, file);
+     if (error) {
+      file.close();
+      somethingWrong("loadJsonParams: Failed to dsrlz config file");
+     }
+     file.close();
+     serializeJsonPretty(doc, Serial);
+     parseRequest(doc);
+   } else {
+     Serial.println("loadJsonParams: config file not found");
+   }
+}
+
+void initPrivate() {
+  File file = SPIFFS.open(privateFile, "r");
+  StaticJsonDocument<256> doc;
+  if (!file) {
+    somethingWrong("Failed to open private file");
+  } 
+  DeserializationError error = deserializeJson(doc, file);
+  if (error) {
+    file.close();
+    somethingWrong("Failed to dsrlz private file");
+  }
+  file.close();
+  strlcpy(mqtt.host, doc["mqtt_server"], sizeof(mqtt.host));
+  strlcpy(mqtt.login, doc["mqtt_login"], sizeof(mqtt.login));
+  strlcpy(mqtt.pass, doc["mqtt_pass"], sizeof(mqtt.pass));
+  mqtt.port = doc["mqtt_port"];
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  DynamicJsonDocument doc(jsonSize);
+  DeserializationError error = deserializeJson(doc, payload, length);
+  if (error) {
+    somethingWrong("Failed to dsrlz mqtt message");
+  }
+
+  parseRequest(doc);
+  JsonObject jobj = doc.as<JsonObject>();
+  saveJsonParams(jobj);
 }
 
 void reconnect() {
@@ -157,6 +236,18 @@ void reconnect() {
       delay(5000);
     }
   }
+}
+
+void stepperCallback() {
+  is_running = false;
+  is_opened = !step_last_dir;
+  step_callback = false;
+  
+  StaticJsonDocument<100> doc;
+  doc["is_running"] = false;
+  doc["is_opened"] = is_opened;
+  JsonObject jo = doc.as<JsonObject>();
+  saveJsonParams(jo);
 }
 
 void setup() {
@@ -178,9 +269,9 @@ void setup() {
     somethingWrong("Failed to mount file system");
   }
   initPrivate();
-
   client.setServer(mqtt.host, mqtt.port);
   client.setCallback(callback);
+  loadJsonParams();
 }
 
 void loop() {
@@ -188,5 +279,9 @@ void loop() {
     reconnect();
   }
   client.loop();
+
+  if (stepper.distanceToGo() == 0 && step_callback) {
+    stepperCallback();
+  }
   stepper.run();
 }
